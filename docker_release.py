2ec5bd7d1bb535d931d589b3c0f9ce17e01c03e6
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-28 02:05:05 +0300

    better script

--- docker_release.py
+++ docker_release.py
@@ -3,13 +3,50 @@
 import subprocess
 import config.version
 
+# subprocess.check_call([
+#    "hub",
+#    "release",
+#    "create",
+#    "-a",
+#    "dist/pyflexebs-{}".format(config.version.version_str),
+#    "-m",
+#    "version {}".format(config.version.version_str),
+#    config.version.version_str,
+# ])
+
 subprocess.check_call([
-    "hub",
-    "release",
-    "create",
-    "-a",
-    "dist/pyflexebs-{}".format(config.version.version_str),
-    "-m",
-    "version {}".format(config.version.version_str),
+    "git",
+    "tag",
     config.version.version_str,
+])
+subprocess.check_call([
+    "git",
+    "push",
+    "--tags",
+])
+subprocess.check_call([
+    "github-release",
+    "release",
+    "--user",
+    "veltzer",
+    "--repo",
+    "pyflexebs",
+    "--tag",
+    config.version.version_str,
+    "--name",
+    "version {}".format(config.version.version_str),
+])
+subprocess.check_call([
+    "github-release",
+    "upload",
+    "--user",
+    "veltzer",
+    "--repo",
+    "pyflexebs",
+    "--tag",
+    config.version.version_str,
+    "--name",
+    "pyflexebs-{}".format(config.version.version_str),
+    "--file",
+    "dist/pyflexebs-{}".format(config.version.version_str),
 ])
