cba9d24b27ce42de76d0479ede7b6916b66170e5
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-16 19:57:12 +0200

    version 85

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
-version = (0, 0, 84)
+version = (0, 0, 85)
 version_str = ".".join(str(x) for x in version)
--- pyflexebs/static.py
+++ pyflexebs/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.84"
+VERSION_STR = "0.0.85"
 DESCRIPTION = "pyflexebs will allow you to monitor and expand/contract you ebs in aws"
 APP_NAME = "pyflexebs"
 LOGGER_NAME = "pyflexebs"
