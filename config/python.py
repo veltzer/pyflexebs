ac90bee0d8c9b87dddda45694a9882ffb43e55b8
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 19:12:40 +0300

    new config

--- config/python.py
+++ config/python.py
@@ -1,7 +1,3 @@
-import config.project
-
-package_name = config.project.project_name
-
 console_scripts = [
     "pyflexebs=pyflexebs.main:main",
 ]
@@ -36,8 +32,3 @@ test_requires = [
     "flake8",
     "pymakehelper",
 ]
-
-python_requires = ">=3.10"
-
-test_os = ["ubuntu-22.04"]
-test_python = ["3.10"]
